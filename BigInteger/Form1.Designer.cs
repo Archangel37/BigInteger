2ecb0f899c8365a89ad356466e398be807ca124d
Archangel <djarchangel3737@gmail.com>
2017-08-03 11:30:26 +0300

    some changes to En, moving richTextBox to add new text-boxes

--- BigInteger/Form1.Designer.cs
+++ BigInteger/Form1.Designer.cs
@@ -34,19 +34,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(301, 13);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Вычисление";
+            this.button1.Text = "Calculating";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(40, 42);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 127);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(721, 526);
+            this.richTextBox1.Size = new System.Drawing.Size(784, 526);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
@@ -54,11 +54,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(808, 610);
+            this.ClientSize = new System.Drawing.Size(808, 665);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BigInteger";
             this.ResumeLayout(false);
 
         }
